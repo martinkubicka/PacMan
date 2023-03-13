c5b448752ab1bc7ecb517f6ff7f228d05adc271e
martinkubicka <martinkubicka22@gmail.com>
2023-03-13 21:08:24 +0100

    fix key path

--- src/key.cpp
+++ src/key.cpp
@@ -11,7 +11,7 @@
 #include "key.h"
 
 Key::Key(QGraphicsScene* scene, int x1, int y1, int x2, int y2, QString srcPath) : Field (x1, y1, x2, y2, KEY) {
-    QImage keyImage(srcPath + "Man/images/key.png");
+    QImage keyImage(srcPath + "/images/key.png");
     keyImage = keyImage.scaled(QSize(x2-x1, y2-y1), Qt::KeepAspectRatio);
 
     QGraphicsPixmapItem* keyItem = new QGraphicsPixmapItem(QPixmap::fromImage(keyImage));
